1b6cb6c21cb6e8f3a6eda7760e4e22d37d55ab83
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-20 02:52:54 +0600

    delete book added

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -60,26 +60,42 @@ booksRoutes.get("/books/:bookId", async (req: Request, res: Response) => {
     const book = await Book.findById(bookId);
 
     res.status(200).json({
-        success: true,
-        message: "Book retrieved successfully",
-        data: book
-    })
-
+      success: true,
+      message: "Book retrieved successfully",
+      data: book,
+    });
   } catch (error) {
     console.log(error);
   }
 });
 
-
 // put method: update book
-booksRoutes.put('/books/:bookId', async (req: Request, res: Response) => {
-    const bookId = req.params.bookId;
-    const updatedBody = req.body;
-    const book = await Book.findByIdAndUpdate(bookId, updatedBody, {new: true});
+booksRoutes.put("/books/:bookId", async (req: Request, res: Response) => {
+  const bookId = req.params.bookId;
+  const updatedBody = req.body;
+  const book = await Book.findByIdAndUpdate(bookId, updatedBody, { new: true });
 
-    res.status(201).json({
+  res.status(201).json({
+    success: true,
+    message: "Book updated successfully",
+    data: book,
+  });
+});
+
+// delete method: delete a book
+booksRoutes.delete("/books/:bookId", async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.bookId;
+
+    const book = await Book.findByIdAndDelete(bookId);
+
+    res.status(200).json({
         success: true,
-        message: "Book updated successfully",
-        data: book
+        message: "Book deleted successfully",
+        data: null
     })
+
+  } catch (error) {
+    console.log(error);
+  }
 });
