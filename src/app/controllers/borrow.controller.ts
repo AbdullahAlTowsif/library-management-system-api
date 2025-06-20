e1c7811f5394707a358652d6ba9517ab8005b65c
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-21 01:26:43 +0600

    borrow book summary retrieved

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -3,7 +3,10 @@ import { Borrow } from "../models/borrow.model";
 
 export const borrowRoutes = express.Router();
 
-borrowRoutes.post("/", async (req: Request, res: Response, next: NextFunction) => {
+// post request: borrow books
+borrowRoutes.post(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { book, quantity } = req.body;
 
@@ -20,6 +23,52 @@ borrowRoutes.post("/", async (req: Request, res: Response, next: NextFunction) =
         message: "Book borrowed successfully",
         success: true,
         data: borrow,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// get request: borrow books summary
+borrowRoutes.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const summary = await Borrow.aggregate([
+        {
+          $group: {
+            _id: "$book",
+            totalQuantity: { $sum: "$quantity" },
+          },
+        },
+        {
+          $lookup: {
+            from: "books",
+            localField: "_id",
+            foreignField: "_id",
+            as: "bookDetails",
+          },
+        },
+        {
+          $unwind: "$bookDetails",
+        },
+        {
+          $project: {
+            _id: 0,
+            book: {
+              title: "$bookDetails.title",
+              isbn: "$bookDetails.isbn",
+            },
+            totalQuantity: 1,
+          },
+        },
+      ]);
+
+      res.status(200).json({
+        success: true,
+        message: "Borrowed books summary retrieved successfully",
+        data: summary,
       });
     } catch (error) {
       next(error);
